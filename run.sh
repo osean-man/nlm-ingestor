dc7e7a0c35bf69310b3152221a429e41c518edb2
Osean Man <60236923+osean-man@users.noreply.github.com>
2024-05-18 20:06:42 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# latest version of java and a python environment where requirements are installed is required
+
 nohup java -jar jars/tika-server-standard-nlm-modified-2.4.1_v6.jar > /dev/null 2>&1 &
-gunicorn --worker-class gevent --bind 0.0.0.0:5001 nlm_ingestor.ingestion_daemon:app
 
-
+gunicorn --worker-class gevent --bind 0.0.0.0:5001 "nlm_ingestor.ingestion_daemon.__main__:app"
